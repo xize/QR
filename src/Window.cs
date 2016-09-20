ee5e97d85ff3ee83ea3dac3c5b6f32ffc46b879f
xize <glucassen@live.nl>
2016-09-20 14:27:13 +0200

    when minimizing in tray, remove QR code

--- src/Window.cs
+++ src/Window.cs
@@ -122,6 +122,7 @@ namespace src.QR_GEN
 
             if(e.CloseReason == CloseReason.UserClosing)
             {
+                picture.Image = null;
                 e.Cancel = true;
                 this.Visible = false;
                 notifyIcon.Visible = true;
