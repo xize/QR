24cecbd21ce65441bb57d39ab092e58529f4cbc2
xize <glucassen@live.nl>
2017-10-23 23:06:04 +0200

    do not run multiple instances

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,12 +9,20 @@ namespace src.QR_GEN
 {
     static class Program
     {
+
+        private static Mutex mutex = new Mutex(false, "35b778e7-1b7d-4494-823f-c15f476729d7");
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
+            if(!mutex.WaitOne(0, false))
+            {
+                MessageBox.Show("can only run a single instance of this program!", "error");
+                return;
+            }
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new Window());
