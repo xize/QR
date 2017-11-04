2dcde5b518f64d43c9de53b7a6ffe29f79e15b93
xize <glucassen@live.nl>
2017-11-04 19:27:28 +0100

    replace zxing library for QRCoder library

--- src/Generator.cs
+++ src/Generator.cs
@@ -1,14 +1,11 @@
-﻿using System;
+﻿using QRCoder;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Net.Http;
 using System.Windows.Forms;
-using ZXing;
-using ZXing.Common;
-using ZXing.QrCode;
-
 namespace src.QR_GEN
 {
     class Generator
@@ -20,18 +17,10 @@ namespace src.QR_GEN
 
         public Image GenerateQR(string text)
         {
-            BarcodeWriter writer = new BarcodeWriter
-            {
-                Format = BarcodeFormat.QR_CODE,
-                Options = new EncodingOptions
-                {
-                    Height = 260,
-                    Width = 260,
-                    Margin = 0
-                }
-            };
-
-            return writer.Write(text);
+            QRCodeGenerator gen = new QRCodeGenerator();
+            QRCodeData data = gen.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            QRCode code = new QRCode(data);
+            return new Bitmap(code.GetGraphic(20), 260, 260);
         }
 
         public Image GenerateAppendedImage(string text, Image orginal)
@@ -53,14 +42,8 @@ namespace src.QR_GEN
 
         [Obsolete]
         public string ReadQR(Image image)
-        {
-            //TODO: check if we want to implement this.
-            Bitmap img = new Bitmap(image);
-            BarcodeReader reader = new BarcodeReader();
-
-            Result result = reader.Decode(img);
-
-            return result.ToString().Trim();
+        {       
+            return null;
         }
 
         public static Generator GetGenerator()
