8843e55d1844a0cf43f7b11c92ce74bfa27b6358
xize <glucassen@live.nl>
2017-12-03 15:34:52 +0100

    make the font size smaller for assci QR

--- src/AsciiWindow.Designer.cs
+++ src/AsciiWindow.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.richTextBox1.BackColor = System.Drawing.Color.Black;
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 3F);
             this.richTextBox1.ForeColor = System.Drawing.Color.White;
             this.richTextBox1.Location = new System.Drawing.Point(0, 0);
             this.richTextBox1.Name = "richTextBox1";
